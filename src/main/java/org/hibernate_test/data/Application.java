3ab14f6f685366f5b4476a777d90b8891b9cd678
Hint1k <com>
2023-06-20 15:31:29 +0400

    Testing Hibernate 6 Project ver 0.1

---
+++ src/main/java/org/hibernate_test/data/Application.java
@@ -0,0 +1,40 @@
+package org.hibernate_test.data;
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate_test.data.entities.User;
+
+public class Application {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.flush();
+        User user1 = new User();
+        User user2 = new User();
+        user1.setUserId(3L);
+        user2.setUserId(4L);
+        user1.setFirst_name("C");
+        user2.setFirst_name("D");
+        user1.setBirthdate(new Date());
+        user2.setBirthdate(new Date());
+        user1.setLast_name("C");
+        user2.setLast_name("D");
+        user1.setCreateDate(new Date());
+        user2.setCreateDate(new Date());
+        user1.setCreatedBy("F");
+        user2.setCreatedBy("F");
+        user1.setLastUpdatedBy("F");
+        user2.setLastUpdatedBy("F");
+        user1.setLastUpdatedDate(new Date());
+        user2.setLastUpdatedDate(new Date());
+
+        session.merge(user1);
+        session.merge(user2);
+        session.getTransaction().commit();
+
+        session.close();
+    }
+}
